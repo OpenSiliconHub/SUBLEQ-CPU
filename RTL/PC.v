faeeb69cc204d86d30b4df960f4efb53bce4e519
ABHILASH MADDINENI <214151262+MrAbhi19@users.noreply.github.com>
2026-06-06 11:59:08 +0530

    Add Program Counter module

---
+++ RTL/PC.v
@@ -0,0 +1,19 @@
+module PC (
+    input  wire        clk,
+    input  wire        rst,
+    input  wire        en,
+    input  wire        jump,
+  input  wire [15:0]   jump_addr,
+    output reg  [15:0] pc
+);
+
+  always @(posedge clk or posedge rst) begin
+    if (rst)
+        pc <= 16'h0000;
+    else if (jump)
+        pc <= jump_addr;    
+    else if (en)
+        pc <= pc + 16'd1;    
+end
+
+endmodule
