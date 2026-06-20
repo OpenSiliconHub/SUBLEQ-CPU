f0487c2498b70192f3ef8d6b164716a641d43797
dion <dion.barrick@gmail.com>
2026-06-19 21:43:59 -0400

    add generic for file input for preloading memory from file

--- RTL/VHDL/single_port_ram.vhd
+++ RTL/VHDL/single_port_ram.vhd
@@ -1,11 +1,13 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use std.textio.all;
 
 entity single_port_ram is
   generic (
-    G_DATA_WIDTH : natural := 8;
-    G_ADDR_WIDTH : natural := 10
+    G_DATA_WIDTH : natural := 16;
+    G_ADDR_WIDTH : natural := 16
+    G_INIT_MEM   : string  := "program.hex"
   );
   port (
     i_clk  : in  std_logic;
@@ -20,7 +22,32 @@ architecture rtl of single_port_ram is
   ----------------------------------------------------------------------------------------------------------------------
   -- mem_proc
   type ram_array_t is array (0 to (2 ** G_ADDR_WIDTH) - 1) of std_logic_vector(G_DATA_WIDTH - 1 downto 0);
-  signal ram_block : ram_array_t;
+  ----------------------------------------------------------------------------------------------------------------------
+  -- Function to preload memory from file
+  impure function init_ram_f(
+    i_filename : in string
+  ) return ram_array_t is
+
+    file mem_file      : text open read_mode is i_filename;
+    variable mem_line  : line;
+    variable mem_value : ram_array_t;
+
+  begin
+
+    for ii in ram_array_t'range loop
+      if (not endfile(mem_file)) then
+        readline(mem_file, mem_line);
+        hread(mem_line, mem_value(ii));
+      else
+        mem_value(ii) := (others => '0');
+      end if;
+    end loop;
+
+    return mem_value;
+  end function;
+  ----------------------------------------------------------------------------------------------------------------------
+  -- Assign ram_block
+  signal ram_block : ram_array_t := init_ram_f(G_INIT_MEM);
 
 begin
 
